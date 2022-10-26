f7387bae7866109d2ec5a11cda557b88dc63d9b9
Thisuru Deesan <thisuru.d@eyepax.com>
2022-10-27 00:09:49 +0530

    Nav bar position made as Sticky

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,29 +17,27 @@ const style = {
     lineHeight: '150%'
 }
 
-
 export const Navbar = () => {
     return (
-        <AppBar position='static' color='inherit'>
-            <Toolbar>
-                <IconButton size='large' edge='start' color='inherit' aria-label='logo'>
-                    <img src="images/Logo/Black.png" alt="" />
-                </IconButton>
-                <Typography variant='h6' component='div' sx={{ flexGrow: 1 }} color='white'>
-                    POKEMONAPP
-                </Typography>
-                <Stack direction='row' spacing={2}>
-                    <CustomButton style={style} color='inherit' size='large'>Home</CustomButton>
-                    <CustomButton style={style} color='inherit' size='large'>About</CustomButton>
-                    <CustomButton style={style} color='inherit' size='large'>Services</CustomButton>
-                    <CustomButton style={style} color='inherit' size='large'>Shop</CustomButton>
+            <AppBar position='sticky' color='inherit'>
+                <Toolbar>
+                    <IconButton size='large' edge='start' color='inherit' aria-label='logo'>
+                        <img src="images/Logo/Black.png" alt="" />
+                    </IconButton>
+                    <Typography variant='h6' component='div' sx={{ flexGrow: 1 }} color='white'>
+                        POKEMONAPP
+                    </Typography>
+                    <Stack direction='row' spacing={2}>
+                        <CustomButton style={style} color='inherit' size='large'>Home</CustomButton>
+                        <CustomButton style={style} color='inherit' size='large'>About</CustomButton>
+                        <CustomButton style={style} color='inherit' size='large'>Services</CustomButton>
+                        <CustomButton style={style} color='inherit' size='large'>Shop</CustomButton>
 
-                    <ThemeProvider theme={theme}>
-                        {/* <Button variant="contained" color='primary' size='large'>Booking</Button> */}
-                        {CustomButton && <CustomButton variant='contained' color='primary' size='large'>Booking</CustomButton>}
-                    </ThemeProvider>
-                </Stack>
-            </Toolbar>
-        </AppBar>
+                        <ThemeProvider theme={theme}>
+                            {CustomButton && <CustomButton variant='contained' color='primary' size='large'>Booking</CustomButton>}
+                        </ThemeProvider>
+                    </Stack>
+                </Toolbar>
+            </AppBar>
     )
 }
