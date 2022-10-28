6fa236fa697cd7aeba87c3ba5d651d29bf379632
Thisuru Deesan <thisuru.d@eyepax.com>
2022-10-28 11:33:28 +0530

    Service Details page UI styles improved

--- src/modules/serviceDetails/ServiceDetails.tsx
+++ src/modules/serviceDetails/ServiceDetails.tsx
@@ -2,6 +2,29 @@ import "../../App.css";
 import "../../components/HeroSection.css";
 import { services } from "../../utils/Constants";
 import { Grid, Typography, Stack } from '@mui/material'
+import { CustomButton } from "../../components/CustomButton";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000',
+    },
+  },
+});
+
+const style1 = {
+  padding: '8px 20px',
+  outline: 'none',
+  cursor: 'pointer',
+  border: '1px solid var(--primary)',
+  borderRadius: '5px',
+  fontFamily: 'Poppins',
+  fontWeight: '400',
+  fontSize: '14px',
+  textTransform: 'capitalize',
+  width: '75%'
+}
 
 const ServiceDetails = () => {
   return (
@@ -13,18 +36,30 @@ const ServiceDetails = () => {
               <Typography
                 variant="h1"
                 style={{ color: 'black', lineHeight: '80px' }}>
-                  Haircut
-              </Typography>
-              
-              <Typography
-                variant="subtitle2"
-                fontFamily='Poppins'
-                fontWeight='400'
-                lineHeight='150%'
-                style={{ color: "black" }}>
-                  {services[0]?.description}
+                Haircut
               </Typography>
 
+              <Stack spacing={4}>
+                <Typography
+                  variant="subtitle2"
+                  fontFamily='Poppins'
+                  fontWeight='400'
+                  lineHeight='150%'
+                  style={{ color: "black" }}
+                >
+                  {services[0]?.description}
+                </Typography>
+
+                <ThemeProvider theme={theme}>
+                  <CustomButton
+                    variant='contained'
+                    size='large'
+                    style={style1}
+                  >
+                    Make a Reservation
+                  </CustomButton>
+                </ThemeProvider>
+              </Stack>
             </Stack>
           </Stack>
         </Grid>
